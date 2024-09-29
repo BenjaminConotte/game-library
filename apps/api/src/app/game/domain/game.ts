d7415c8fdaefae9cbe18978d342b22f71e76647d
Benjamin Conotte <conotte.benjamin@gmail.com>
2024-09-29 21:53:55 +0200

    🚧  Define Game entity

--- apps/api/src/app/game/domain/game.ts
+++ apps/api/src/app/game/domain/game.ts
@@ -3,18 +3,29 @@ type GameProps = {
   id: string;
   name: string;
   type: string;
+  minPlayers: number;
+  maxPlayers: number;
   createdAt: Date;
 };
 export class Game {
-  private constructor(private readonly props: GameProps) {}
-
   static create(props: CreateGameProps): Game {
     return new Game({ ...props, id: '1', createdAt: new Date() });
   }
+  private constructor(private readonly props: GameProps) {}
+
   get name(): string {
     return this.props.name;
   }
   get type(): string {
     return this.props.type;
+  }
+  get minPlayers(): number {
+    return this.props.minPlayers;
+  }
+  get maxPlayers(): number {
+    return this.props.maxPlayers;
+  }
+  get createdAt(): Date {
+    return this.props.createdAt;
   }
 }
--- apps/api/src/app/game/domain/game.unit.spec.ts
+++ apps/api/src/app/game/domain/game.unit.spec.ts
@@ -5,12 +5,18 @@ describe('Game', () => {
     const props = {
       name: 'Game 1',
       type: 'Deck building',
+      minPlayers: 2,
+      maxPlayers: 4,
     };
     // Act
     const game = Game.create(props);
     // Assert
     expect(game).toBeDefined();
+    expect(game).toBeInstanceOf(Game);
     expect(game.name).toBe('Game 1');
     expect(game.type).toBe('Deck building');
+    expect(game.minPlayers).toBe(2);
+    expect(game.maxPlayers).toBe(4);
+    expect(game.createdAt).toBeDefined();
   });
 });
