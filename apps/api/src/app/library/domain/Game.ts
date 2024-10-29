cdff7b6401dca07f190727756d052b262f0f5982
Benjamin Conotte <conotte.benjamin@gmail.com>
2024-10-29 03:49:22 +0100

    🚧 Define Game Name

--- apps/api/src/app/library/domain/Game.ts
+++ apps/api/src/app/library/domain/Game.ts
@@ -1,12 +1,13 @@
 import { GameProps } from './Game.props';
+import { GameName } from './GameName';
 
 export class Game {
-  private _name: string;
+  private _name: GameName;
 
   protected constructor(props: GameProps) {
     this.name = props.name;
   }
   protected set name(name: string) {
-    this._name = name;
+    this._name = new GameName(name);
   }
 }
---
+++ apps/api/src/app/library/domain/GameName.ts
@@ -0,0 +1,14 @@
+export class GameName {
+  private _value: string;
+  constructor(name: string) {
+    if (name === null) throw new Error('Game name is required');
+    if (name.length < 3)
+      throw new Error('Game name must be at least 3 characters');
+    if (name.length > 100)
+      throw new Error('Game name must be less than 50 characters');
+    this.value = name;
+  }
+  private set value(name: string) {
+    this._value = name;
+  }
+}
