1cac5064a0810e6eaf3523a158c891f3d4b0c5bf
Benjamin Conotte <conotte.benjamin@gmail.com>
2024-10-31 12:06:58 +0100

    🚧 Improve shared stuff - Define the CommandHandler class and Command interface

--- apps/api/src/Contexts/Shared/App/CommandHandler.ts
+++ apps/api/src/Contexts/Shared/App/CommandHandler.ts
@@ -1,3 +1,5 @@
 import { Command } from './Command';
 
-export abstract class CommandHandler<C extends Command> {}
+export abstract class CommandHandler<C extends Command> {
+  abstract handle(command: C): Promise<void>;
+}
