2adc6920dc56a907e645545ed318871d8b474b13
Benjamin Conotte <conotte.benjamin@gmail.com>
2024-11-13 21:32:42 +0100

    🗑️ Remove reference to class-validator

--- apps/api/src/library/infra/controller/dto/AddGameToLibrary.dto.ts
+++ apps/api/src/library/infra/controller/dto/AddGameToLibrary.dto.ts
@@ -3,27 +3,21 @@ import {
   AddGameToLibraryCommandBody,
 } from '@game-library/library-context/Library/App/UseCase/Add';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsString } from 'class-validator';
 
 export class AddGameToLibraryDto extends AddGameToLibraryCommand {
   @ApiProperty({ description: 'EAN of the game' })
-  @IsString()
   ean: string;
 
   @ApiProperty({ description: 'Name of the game' })
-  @IsString()
   name: string;
 
   @ApiProperty({ description: 'Language of the game name' })
-  @IsString()
   nameLanguage: string;
 
   @ApiProperty({ description: 'Type of the game' })
-  @IsString()
   type: string;
 
   @ApiProperty({ description: 'Is the game cooperative?' })
-  @IsBoolean()
   isCoop: boolean;
   constructor(props: AddGameToLibraryCommandBody) {
     super(props);
